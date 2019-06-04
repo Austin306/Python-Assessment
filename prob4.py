8ff4a9c88e9ed19e907d9341c7baaa2f0663cc90
Austin306 <austin.dsouza@quantiphi.com>
2019-06-04 14:03:04 +0530

    Solution

--- prob4.py
+++ prob4.py
@@ -2,28 +2,30 @@ ConnectionsFormed=[] #Global Connection
 def findConnections(name1, name2):
     if len(ConnectionsFormed)==0:
         connect=[name1,name2]
-        ConnectionsFormed.append(connect)
+        ConnectionsFormed.append(connect)#If a new connection directly append it to Global connection
     else:
         found=1
-        for i in ConnectionsFormed:
-            if name1 in i :
+        for i in ConnectionsFormed:#If connection is present 
+            if name1 in i :#Check if name1 is already present and append the name2 to already present connection 
                 i.append(name2)
                 found=1
                 return
-            elif name2 in i:
+            elif name2 in i:#Check if name2 is already present then add name1 as a new connection to already existing list
                 i.append(name1)
                 found=1
                 return
             else:
                 found=0
         if found==0:
-            connect=[name1,name2]
+            connect=[name1,name2] #If names dont exist create a new connection
             ConnectionsFormed.append(connect)
 
 def printConnections():
+    '''Function to print the connections'''
     for i in ConnectionsFormed:
         print(i)
 def queryConnections(name1, name2):
+    '''Function to check if both the names are interlinked'''
     for i in ConnectionsFormed:
         if (name1 in i) and (name2 in i):
            print("Yes")
@@ -37,7 +39,7 @@ for i in range(n_entries):#For loop to group the entries
     print("enter entries")
     entries=list(input().split(' '))
     findConnections(entries[0],entries[1])
-    entries.clear()
+    entries.clear()#refresh the entries 
 printConnections()
 print(len(ConnectionsFormed))
 
